a7c068c209778a5c8ad675ce89a430284414672b
chenzhicun <703388573@qq.com>
2021-06-02 01:11:51 +0800

    now you can decide read image in GRAY mode or RGB mode by in_channel.

--- utils/dataset.py
+++ utils/dataset.py
@@ -6,15 +6,17 @@ import torch
 from torch.utils.data import Dataset
 import logging
 from PIL import Image
+import cv2
 
 from .argumentation import data_argument
 
 
 class MIS_Dataset(Dataset):
-    def __init__(self, imgs_dir, masks_dir, argument=True, type='train', val_percent=0.1) -> None:
+    def __init__(self, imgs_dir, masks_dir, argument=True, type='train', val_percent=0.1, in_channel=1) -> None:
         self.imgs_dir = imgs_dir
         self.masks_dir = masks_dir
         self.argument = argument
+        self.in_channel=in_channel
 
         self.ids = [splitext(file)[0] for file in listdir(
             imgs_dir) if not file.startswith('.')]
@@ -31,25 +33,6 @@ class MIS_Dataset(Dataset):
 
     def __len__(self):
         return len(self.ids)
-
-    # @classmethod
-    # def preprocess(cls, pil_img, scale):
-    #     w, h = pil_img.size
-    #     new_w, new_h = int(scale*w), int(scale*h)
-    #     assert new_h > 0 and new_w > 0, 'new image size is too small!'
-    #     pil_img = pil_img.resize((new_w, new_h))
-
-    #     img_nd = np.array(pil_img)
-
-    #     if len(img_nd.shape) == 2:
-    #         img_nd = np.expand_dims(img_nd, axis=2)
-
-    #     # H x W x C => C x H x W
-    #     img_trans = img_nd.transpose((2, 0, 1))
-    #     if img_trans.max() > 1:
-    #         img_trans = img_trans/255
-
-    #     return img_trans
 
     @classmethod
     def img_transform(cls, img):
@@ -69,15 +52,33 @@ class MIS_Dataset(Dataset):
         idx = self.ids[index]
         mask_file = f'{self.masks_dir}/{idx}.png'
         img_file = f'{self.imgs_dir}/{idx}.png'
+        if self.in_channel==1:
+            mask = Image.open(mask_file)
+            img = Image.open(img_file)
+            if self.argument:
+                img, mask = data_argument(img, mask)
+            mask = self.img_transform(mask)
+            img = self.img_transform(img)
+            img = (img - 0.5052) / 0.1678
+        if self.in_channel==3:
+            mask = Image.open(mask_file)
+            img = Image.open(img_file)
+            if self.argument:
+                img, mask = data_argument(img, mask)
+            mask = self.img_transform(mask)
+            img = np.array(img)
+            img = 255 - img
+            img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+            img = img / 255
+            
+            _std = np.array([0.229, 0.224, 0.225]).reshape((1,1,3))
+            _mean = np.array([0.485, 0.456, 0.406]).reshape((1,1,3))
 
-        mask = Image.open(mask_file)
-        img = Image.open(img_file)
-        # if self.argument:
-        #     img, mask = data_argument(img, mask)
-        mask = self.img_transform(mask)
-        img = self.img_transform(img)
+            img = (img - _mean) / _std
+            img = img.transpose((2,0,1))
 
-        assert img.size == mask.size, f'Image and mask {idx} should be the same size, but are {img.size} and {mask.size}'
+
+        assert img.shape[1:] == mask.shape[1:], f'Image and mask {idx} should be the same size, but are {img.size} and {mask.size}'
 
         return {
             'image': torch.from_numpy(img).type(torch.FloatTensor),
